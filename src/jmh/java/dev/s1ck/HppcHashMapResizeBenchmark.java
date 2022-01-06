dbe846e3fb638571303aec5445aa18e1314a3fb6
Martin Junghanns <m.junghanns@mailbox.org>
2022-01-06 13:50:13 +0100

    Add HppcHashMapResizeBenchmark

--- src/jmh/java/dev/s1ck/HppcHashMapResizeBenchmark.java
+++ src/jmh/java/dev/s1ck/HppcHashMapResizeBenchmark.java
@@ -5,34 +5,32 @@ import org.openjdk.jmh.annotations.*;
 
 import java.util.HashMap;
 
-/**
- * https://richardstartin.github.io/posts/5-java-mundane-performance-tricks#size-hashmaps-whenever-possible
- */
 @Fork(1)
 @Warmup(iterations = 5, time = 1)
 @Measurement(iterations = 10, time = 1)
 @State(Scope.Benchmark)
-public class HashMapResizeBenchmark {
+public class HppcHashMapResizeBenchmark {
 
-    @Param({"10", "14"})
+    @Param({"10", "24"})
     int keys;
 
-    // cap 16 (16)
+    // cap 10 (16)
     // keys 10 => resizeAt = 12 -> no resize
-    // keys 24 => resizeAt = 12 -> resize -> resizeAt = 24
+    // keys 26 => resizeAt = 12 -> resize -> resizeAt = 24
     //
     // cap 24 (32)
     // keys 10 => resizeAt = 24 -> no resize
-    // keys 24 => resizeAt = 24 -> no resize
-    @Param({"16", "24"})
+    // keys 26 => resizeAt = 24 -> no resize
+    @Param({"10", "24"})
     int capacity;
 
     @Benchmark
-    public HashMap<Integer, Integer> loadHashMap() {
-        HashMap<Integer, Integer> map = new HashMap<>(capacity);
+    public IntIntHashMap loadHppcHashMap() {
+        IntIntHashMap map = new IntIntHashMap(capacity);
         for (int i = 0; i < keys; ++i) {
             map.put(i, i);
         }
         return map;
     }
+
 }
